aec0c24f2c988c1e02e93770f96ce56e83adb83a
Felipe <fquiceno1@misena.edu.co>
2022-07-13 13:26:29 -0500

    Creacion de los servicios para login

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,8 @@
+export interface User{
+  id:string,
+  email:string,
+  password:string
+}
+
+//DTO
+export interface CreateUserDTO extends Omit<User,'id'>{}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {environment} from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  /*
+    cremaos la logica para el Login
+  */
+  //URL Base
+  private apiUrl = `${environment.API_URL}/api/auth`;
+
+  constructor(
+    private http: HttpClient //Inyeccion de dependencia
+  ) { }
+
+  login(email:string,password:string){
+
+    return this.http.post(`${this.apiUrl}/login`, {email,password});
+  }
+
+  profile(){
+
+    return this.http.get(`${this.apiUrl}/profile`);
+  }
+}
