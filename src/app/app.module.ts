c1223ba769fd847ca512f79570b7904d7be52401
Felipe <fquiceno1@misena.edu.co>
2022-07-13 14:23:42 -0500

    Interceptores son el mecanismo de interceptar y mutar las peticiones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';// MODULO PARA SOLICITUDES HTTP
 import { SwiperModule } from 'swiper/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImgComponent } from './components/img/img.component';
@@ -13,6 +11,8 @@ import { NavComponent } from './components/nav/nav.component';
 import { ReversePipe } from './pipes/reverse.pipe';
 import { TimeAgoPipe } from './pipes/time-ago.pipe';
 import { HighlightDirective } from './directives/highlight.directive';
+import { HttpClientModule , HTTP_INTERCEPTORS} from '@angular/common/http';// MODULO PARA SOLICITUDES HTTP
+import { TimeInterceptor } from './interceptors/time.interceptor';
 
 @NgModule({
   declarations: [
@@ -32,7 +32,9 @@ import { HighlightDirective } from './directives/highlight.directive';
     FormsModule,
     SwiperModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass:TimeInterceptor, multi:true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
