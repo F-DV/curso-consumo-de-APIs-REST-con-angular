ed6f4df7cdb2be0448230f7d40473f12f787a54a
Felipe <fquiceno1@misena.edu.co>
2022-07-13 13:45:43 -0500

    login y manejo de autenticacion

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment} from '../../environments/environment';
+import { Auth } from '../models/auth.model';
 
 @Injectable({
   providedIn: 'root'
@@ -19,11 +20,10 @@ export class AuthService {
 
   login(email:string,password:string){
 
-    return this.http.post(`${this.apiUrl}/login`, {email,password});
+    return this.http.post<Auth>(`${this.apiUrl}/login`, {email,password});
   }
 
   profile(){
-
     return this.http.get(`${this.apiUrl}/profile`);
   }
 }
